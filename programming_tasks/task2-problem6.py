4d5006af913ad84bbd2618b88b0db5c88540c52f
Bhadra Suresh <bhdrsuresh@gmail.com>
2025-07-07 17:51:35 +0530

    task2-problem6

---
+++ programming_tasks/task2-problem6.py
@@ -0,0 +1,8 @@
+# 6.Access Specific Elements from a List
+numbers = [4, 8, 15, 16, 23]
+first = numbers[0]
+middle = numbers[2]
+last = numbers[-1]
+print(f"First: {first}")
+print(f"Middle: {middle}")
+print(f"last: {last}")
