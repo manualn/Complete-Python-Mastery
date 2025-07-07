1d004721e1fb2e22ae01a30cd7f4a195c029ea66
Bhadra Suresh <bhdrsuresh@gmail.com>
2025-07-07 13:22:22 +0530

    task2-problem2

--- programming_tasks/task2_problem1.py
+++ programming_tasks/task2_problem1.py
@@ -1,5 +1,8 @@
+# 1. Show How a Sum Builds Up Step-by-Step
 n = int(input("enter a number: "))
 sum = 0
 for i in range(n+1):
     sum += i
     print(f"sum = sum + {i} -> {sum}")
+
+print(f"final sum = {sum}")
---
+++ programming_tasks/task2_problem2.py
@@ -0,0 +1,5 @@
+# 2. Print Each Letter of a Word by Index
+x = input("enter a word: ")
+l = len(x)
+for i in range(l):
+    print(f"Character at index {i}:", x[i])
