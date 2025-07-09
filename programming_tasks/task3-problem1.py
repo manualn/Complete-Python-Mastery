3c89c9e1720a6fc94f8e856f46c514013e88bb85
Bhadra Suresh <bhdrsuresh@gmail.com>
2025-07-09 19:02:54 +0530

    added comments

--- programming_tasks/task3-problem1.py
+++ programming_tasks/task3-problem1.py
@@ -1,11 +1,13 @@
 # 1.Count Length of a String Without len() 
+# input a word
 text = input("enter a word: ")
+# define a function 
 def count_length(text):
     count = 0
     for ch in text:
         count += 1
     return count       
-
+#print length of the word
 print(count_length(text))
 
 
